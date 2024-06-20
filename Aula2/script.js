a3466ff77cf3a845dd46959f8be3c177435a1aa1
Wellington Lima <wellington.lima98@hotmail.com>
2024-06-19 21:29:55 -0300

    adições

---
+++ Aula2/script.js
@@ -0,0 +1,11 @@
+//var x = 'oi'; não é recomendado
+let y = 'oi de novo';
+const z = 'oi mais uma vez';//variavel que não altera, constante
+
+y = 'Aula 2'
+document.querySelector('h1').innerHTML = y;
+document.querySelector('h2').innerHTML = z;
+//document.getElementById('h1').style.color = 'red';
+//document.getElementById('#link2').style.backgroundColor = 'blue';
+document.querySelector('h1').style.color='red'; // atv1
+document.querySelector('h2')
