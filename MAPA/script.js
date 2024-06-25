186fc2f617086311af55d1002e3b57ed477fee69
Wellington Lima <wellington.lima98@hotmail.com>
2024-06-24 21:45:29 -0300

    Upgrade no código

--- MAPA/script.js
+++ MAPA/script.js
@@ -1,15 +1,15 @@
-var homem;
-var mulher;
-var crianca;
+function carne(homem, mulher, crianca){
+    return (homem * 0.5)+(mulher * 0.3)+(crianca * 0.2);
+ }
 
 const form = document.querySelector('form')
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-    homem=parseFloat(document.getElementById('homens').value);
-    mulher=parseFloat(document.getElementById('mulheres').value);
-    crianca=parseFloat(document.getElementById('criancas').value);
-})
+    const homem=parseFloat(document.getElementById('homens').value);
+    const mulher=parseFloat(document.getElementById('mulheres').value);
+    const crianca=parseFloat(document.getElementById('criancas').value);
 
-function carne(homem, mulher, crianca){
-   return (homem * 0.5)+(mulher * 0.3)+(crianca * 0.2);
-}
+    const totalCarne = carne(homem, mulher, crianca);
+
+    document.getElementById('resultado').innerText = `${totalCarne}kg de carne bovina`;
+});
