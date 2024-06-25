ba4caa627618c338236e5c9c339916c11e5deeeb
Wellington Lima <wellington.lima98@hotmail.com>
2024-06-25 20:15:25 -0300

    atualização

--- MAPA/script.js
+++ MAPA/script.js
@@ -1,7 +1,22 @@
 function carne(homem, mulher, crianca){
-    return (homem * 0.5)+(mulher * 0.3)+(crianca * 0.2);
+    return ((homem * 0.5)+(mulher * 0.3)+(crianca * 0.2)).toFixed(1);
  }
 
+function frango(homem, mulher, crianca){
+    return ((homem * 0.2)+(mulher * 0.2)+(crianca * 0.1)).toFixed(1);
+ }
+
+function linguica(homem, mulher, crianca){
+    return ((homem * 0.2)+(mulher * 0.2)+(crianca * 0.2)).toFixed(1);
+ }
+
+function refrigerante(homem, mulher, crianca){
+    return ((homem * 0.3)+(mulher * 0.4)+(crianca * 0.2)).toFixed(1);
+ }
+
+function cerveja(homem, mulher){
+    return ((homem * 0.8)+(mulher * 0.5)).toFixed(1);
+ }
 const form = document.querySelector('form')
 form.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -10,6 +25,14 @@ form.addEventListener('submit', (event) => {
     const crianca=parseFloat(document.getElementById('criancas').value);
 
     const totalCarne = carne(homem, mulher, crianca);
+    const totalFrango = frango(homem, mulher, crianca);
+    const totalLinguica = linguica(homem, mulher, crianca);
+    const totalRefri = refrigerante(homem, mulher, crianca);
+    const totalCerveja = cerveja(homem, mulher, crianca);
 
-    document.getElementById('resultado').innerText = `${totalCarne}kg de carne bovina`;
+    document.getElementById('resultado').innerText = `${totalCarne}kg de carne bovina
+    ${totalFrango}kg de Frango
+    ${totalLinguica}kg de Linguiça
+    ${totalRefri}L de Refrigerante
+    ${totalCerveja}L de Cerveja`;
 });
