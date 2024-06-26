2eddb662d4bbfb40c69b486050bb0b7ca35dce1c
Wellington Lima <wellington.lima98@hotmail.com>
2024-06-26 19:37:17 -0300

    Update script.js

--- Aula4/script.js
+++ Aula4/script.js
@@ -1,14 +1,27 @@
 const form = document.querySelector('form');
 
-form.addEventListener('submit', (event)  =>{
+form.addEventListener('submit', (event) => {
     event.preventDefault();
     const idade = document.getElementById('idade');
     const resultado = document.querySelector('#resultado');
 
-    if (idade.value >= 18){
-        resultado.innerHTML = `Pode dirigir!`;
-    }else{
-        resultado.innerHTML = `Não pode dirigir!`;
-    }
+    // resultado.innerHTML = 'Daqui a 5 anos você terá ' + (Number(idade.value) + 5) + ' anos';
+    
+    // if (isNaN(idade.value)) {
+    //     resultado.innerHTML = "A idade tem que ser um número!";
+    // } else if (idade.value >= 18 && idade.value < 80) {
+    //     resultado.innerHTML = "Pode dirigir!";
+    // } else {
+    //     resultado.innerHTML = "Não pode dirigir!";
+    // }
 
-})
+});
+
+
+const cep = document.getElementById('cep');
+cep.addEventListener('input', (event) => {
+    event.target.value = event.target.value.replace(/\D/g, '');
+    if (event.target.value.length > 5) {
+        event.target.value = event.target.value.slice(0,5) + '-' + event.target.value.slice(5,8);
+    }
+});
